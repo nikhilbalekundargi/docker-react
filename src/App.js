291a9cebd967814f0b836ab1b984a68b90ed58ee
Nikhil Balekundargi <nikhil.balekundargi@gmail.com>
2019-09-09 15:10:32 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro">
+	     hi compose 	Hi there!
         </p>
         <a
           className="App-link"
